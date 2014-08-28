3df5f0a2b963b0e13e72a70ac35aec16e4ed2652
Jimmy Hsu <irregular.profit@gmail.com>
2014-08-28 01:08:50 -0700

    initialize app with signin and twilio token retrieval

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,9 +1,25 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.vibration/www/vibration.js",
+        "id": "org.apache.cordova.vibration.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.phonegap.plugins.twilioclient": "1.0.1"
+    "org.apache.cordova.dialogs": "0.2.9",
+    "org.apache.cordova.vibration": "0.3.10"
 }
 // BOTTOM OF METADATA
 });
--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -1,49 +1,126 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+function init() {
+    document.addEventListener("deviceready", deviceReady, true);
+    checkPreAuth();
+    delete init;
+}
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+function deviceReady() {
+    $("#loginForm").on("submit", handleLogin);
+}
 
-        console.log('Received Event: ' + id);
+function checkPreAuth() {
+    var form = $("#loginForm");
+    if (window.localStorage["email"] != undefined && window.localStorage["password"] != undefined) {
+        $("#email", form).val(window.localStorage["email"]);
+        $("#password", form).val(window.localStorage["password"]);
+        handleLogin();
     }
-};
+}
+
+function handleLogin() {
+    var form = $("#loginForm");
+    //disable the button so we can't resubmit while we wait
+    $("#submitButton", form).attr("disabled", "disabled");
+    var u = $("#email", form).val();
+    var p = $("#password", form).val();
+
+    if (u != '' && p != '') {
+        $.post("http://my-med-labs-call-center.herokuapp.com/api/users/sign_in", {
+            user: {
+                email: u,
+                password: p
+            }
+        }, function(res) {
+            if (res["success"]) {
+                //store
+                window.localStorage["email"] = u;
+                window.localStorage["password"] = p;
+                window.localStorage["name"] = res["name"];
+                window.localStorage["slug"] = res["slug"];
+                window.localStorage["token"] = res["token"];
+
+                twilioDeviceReady();
+                $.mobile.changePage( "#dashboardPage", { transition: "slideup", allowSamePageTransition: true} );
+            } else {
+                navigator.notification.alert("Your login failed", function() {});
+            }
+            $("#submitButton").removeAttr("disabled");
+        }, "json");
+    } else {
+        //Thanks Igor!
+        navigator.notification.alert("You must enter a email and password", function() {});
+        $("#submitButton").removeAttr("disabled");
+    }
+    return false;
+}
+
+
+var callLength;
+var currentCalller;
+
+function twilioDeviceReady() {
+    $("#agentSlug").text(window.localStorage["name"]);
+
+    Twilio.Device.setup(window.localStorage["token"], {
+        debug: true
+    });
+}
+
+
+function show_call_end_status(conn) {
+    var functional_id = ("dialog-confirm" + conn.parameters.From).replace(" ", "-");
+    // maybe only close the call that was otherwise picked
+    $("#" + functional_id).dialog("close");
+    $("#" + functional_id).remove();
+
+    var difference = ($.now() - callLength) / 1000;
+    currentCalller = "";
+
+    if (isNaN(difference)) {
+        var message = "Call ended";
+    } else {
+        var message = "Call ended. Call lasted " + difference + " seconds";
+    }
+
+    $("#log").text(message);
+}
+
+function conference(name, number) {
+    $.post("http://my-med-labs-call-center.herokuapp.com/init_conference", {
+        agent: name,
+        from: number
+    });
+}
+
+function dial() {
+    alert(2);
+
+    alert($("#ToNumber").val());
+    params = {
+        "PhoneNumber": $("#ToNumber").val()
+    };
+    Twilio.Device.connect(params);
+}
+
+function check_rooms() {
+    $.post("http://my-med-labs-call-center.herokuapp.com/check_rooms");
+}
+
+function check_logs() {
+    $.post("http://my-med-labs-call-center.herokuapp.com/check_logs");
+}
+
+function hangup() {
+    Twilio.Device.disconnectAll();
+}
+
+
+$("ul#users li").click(function() {
+    conference($(this).attr('rel'), currentCalller);
+});
+
+
+
+
+
+
